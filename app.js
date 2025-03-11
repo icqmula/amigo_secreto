7ad252530c9eb46f931cc2f5652c7cfbad3fbd80
icqmula <53048894+icqmula@users.noreply.github.com>
2025-03-11 08:35:24 -0300

    Update app.js
    Se implemento la función agregarAmigo con las siguientes tareas especificas:
    
    Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
    
    Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
    
    Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
    
    Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
+function agregarAmigo() {
+  const inputAmigo = document.getElementById('amigo');
+  const nombreAmigo = inputAmigo.value.trim();
+
+  if (nombreAmigo === '') {
+    alert('Por favor, ingresa un nombre válido.');
+    return;
+  }
+
+  listaDeAmigos.push(nombreAmigo);
+  inputAmigo.value = '';
+
+  actualizarListaAmigos();
+}
